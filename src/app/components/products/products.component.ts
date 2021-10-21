64bfc0a1fe1be47a81872669a3b8809a0ae6efed
pechkr2020 <pechjie3238@gmail.com>
2021-10-22 00:58:38 +0700

    first commit

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
+import { LocalStorageService } from 'angular-web-storage';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products: any;
+  token?: string;
+
+  constructor(private ps: ProductsService, public local: LocalStorageService,private router: Router) { 
+    try {
+      this.token = this.local.get('user').token
+      this.ps.getAllProducts(this.token).subscribe(
+        data =>{
+          this.products = data;
+        },
+        err =>{
+          this.router.navigate(['/signin'])
+        }
+      );
+    }catch(error){
+      console.log(error);
+      this.router.navigate(['/signin']);
+    }
+  }
+
+  ngOnInit(): void {
+  }
+  
+  signout(){
+    this.local.clear();
+    this.router.navigate(['/signin']);
+  }
+  
+}
